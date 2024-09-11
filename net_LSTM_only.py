f12a3baf5e5654d0bc0044292978f4d54d741ed4
aobshatko <alexey.obshatko@compvisionsys.com>
2024-09-12 01:50:43 +0300

    add cross-validation

--- net_LSTM_only.py
+++ net_LSTM_only.py
@@ -6,19 +6,21 @@ warnings.filterwarnings("ignore", category=UserWarning)
 class StairSafetyNetLSTM(mx.gluon.nn.HybridBlock):
     def __init__(self, LSTM_layer_size, LSTM_num_layers, dropout_rate, sequence_len):
         super().__init__()
+        #self.norm_block = mx.gluon.nn.LayerNorm(axis=2)
         self.LSTM_block = mx.gluon.rnn.LSTM(LSTM_layer_size, LSTM_num_layers, input_size=64*48*17, dropout=dropout_rate, layout='NTC')
-        self.fc_block_1 = mx.gluon.nn.Dense(32, activation="relu")
-        self.drop_1 = mx.gluon.nn.Dropout()
+        self.fc_block_1 = mx.gluon.nn.Dense(8, activation="relu")
         self.fc_block_2 = mx.gluon.nn.Dense(2)
+
+        self.fc_block_2.weight.wd_mult = 0
 
         self.sequence_len = sequence_len
 
     def hybrid_forward(self, x):
+        #x_norm = self.norm_block(x)
         y1 = self.LSTM_block(x)
         y2 = self.fc_block_1(y1)
-        y2_drop = self.drop_1(y2)
-        y3 = self.fc_block_2(y2_drop)
-        return mx.nd.sigmoid(y3)
+        y3 = self.fc_block_2(y2)
+        return y3
 
 if __name__ == "__main__":
     device=mx.gpu()
--- train_stair_safety.py
+++ train_stair_safety.py
@@ -20,7 +20,8 @@ class SequenceAssembler():
         self.data_path = data_path
         self.label = label
         self.seq_counter = 0
-        self.seq_max = 30
+        self.prev_end = 0
+        self.seq_max = 3000
         self.seq_len = seq_len
         self.elements_amount = mx.nd.load(self.data_path)[0].shape[0]
         self.step = 1
@@ -38,14 +39,16 @@ class SequenceAssembler():
         if self.ended:
             raise IndexError("Unused sequences ended")
         data = mx.nd.load(self.data_path)[0]
-        seq_start = random.randint(0, self.elements_amount + self.step - 1 - self.seq_len * self.step)
+        seq_start = self.prev_end
         seq_end = seq_start + self.seq_len * self.step
         sequence = data[seq_start : seq_end : self.step]
         #sequence_norm = normalize(sequence, self.mean, self.std)
         reshaped_sequence = sequence.reshape((1, self.seq_len, 17*64*48))
         self.seq_counter += 1
-        if self.seq_counter == self.seq_max:
+        self.prev_end = seq_end
+        if (self.seq_counter == self.seq_max) or ((seq_end + self.seq_len * self.step) >= (self.elements_amount + self.step - 1)):
             self.seq_counter = 0
+            self.prev_end = 0
             if self.step < self.max_step:
                 self.step += 1
             else:
@@ -73,13 +76,19 @@ def get_data_paths(path, context):
     return labeled_paths
 
 
-def evaluate_test(test_data_paths, net, seq_len, mean, std):
+def evaluate_test(test_labeled_paths, net, seq_len, mean, std):
+    test_paths = list(test_labeled_paths.keys())
+    acc, met = evaluate_val(test_paths, test_labeled_paths, net, seq_len, mean, std)
+    return acc, met
+
+
+def evaluate_val(data_paths, labeled_paths, net, seq_len, mean, std):
     metric_test = mx.metric.MSE()
     accuracy_test = mx.metric.Accuracy()
-    sequence_assemblers = [SequenceAssembler(data_path, test_data_paths[data_path], seq_len, mean, std) 
-                               for data_path in test_data_paths.keys()]
-    while any(assembler.ended == False for assembler in sequence_assemblers):
-        available_assemblers = [assembler for assembler in sequence_assemblers if assembler.ended == False]
+    val_sequence_assemblers = [SequenceAssembler(data_path, labeled_paths[data_path], seq_len, mean, std) 
+                               for data_path in data_paths]
+    while any(assembler.ended == False for assembler in val_sequence_assemblers):
+        available_assemblers = [assembler for assembler in val_sequence_assemblers if assembler.ended == False]
         selected_assembler = random.choice(available_assemblers)
         sequence, label = selected_assembler.get_sequence()
         output = net.hybrid_forward(sequence)
@@ -87,7 +96,7 @@ def evaluate_test(test_data_paths, net, seq_len, mean, std):
         metric_test.update(label, output)
     _, acc = accuracy_test.get()
     _, met = metric_test.get()
-    return acc, met
+    return round(acc, 5), round(met, 5)
 
 
 def train():
@@ -95,22 +104,31 @@ def train():
     TEST_SET = "ndarray-data/test"
     EPOCHS = 200
     BATCH_SIZE = 32
-    SEQUENCE_LEN = 16
+    SEQUENCE_LEN = 8
+    CV_FOLDS = 5
     lr = 0.0004
     epsilon = 2e-07
-    LSTM_layer_size = 128
+    LSTM_layer_size = 32
     LSTM_num_layeres = 3
-    dropout = 0
+    dropout = 1/16
     weight_decay = 0
-    log_interval = 50
+    log_interval = 10
 
     device = mx.gpu()
     net = StairSafetyNetLSTM(LSTM_layer_size, LSTM_num_layeres, dropout, SEQUENCE_LEN)
 
-    train_data_paths = get_data_paths(TRAIN_SET, device)
-    test_data_paths = get_data_paths(TEST_SET, device)
+    train_labeled_paths = get_data_paths(TRAIN_SET, device)
+    test_labeled_paths = get_data_paths(TEST_SET, device)
 
-    mean, std = get_mean_std(train_data_paths)
+    # cross-validation implementation
+    cross_val_paths = list(train_labeled_paths.keys())
+    random.shuffle(cross_val_paths)
+    fold_size = int(len(cross_val_paths) / CV_FOLDS)
+    data_folds = [cross_val_paths[i * fold_size : (i + 1) * fold_size] for i in range(CV_FOLDS - 1)]
+    data_folds.append(cross_val_paths[(CV_FOLDS - 1) * fold_size :])
+
+    #mean, std = get_mean_std(train_labeled_paths)
+    mean, std = 0, 0
 
     net.initialize(mx.init.Xavier(), ctx=device)
     trainer = gluon.Trainer(net.collect_params(), 'adam', {'learning_rate': lr, 'epsilon': epsilon, 'wd': weight_decay})
@@ -122,13 +140,16 @@ def train():
         accuracy.reset()
         metric.reset()
         batch_count = 0
+        val_fold_id = epoch % CV_FOLDS
+        val_fold = data_folds[val_fold_id]
+        train_paths = [data_path for fold in data_folds if fold != val_fold for data_path in fold]
         batch = mx.nd.empty((0, SEQUENCE_LEN, 17*64*48), ctx=device)
         label_batch = mx.nd.empty((0, 2), ctx=device)
-        sequence_assemblers = [SequenceAssembler(data_path, train_data_paths[data_path], SEQUENCE_LEN, mean, std) 
-                               for data_path in train_data_paths.keys()]
+        train_sequence_assemblers = [SequenceAssembler(data_path, train_labeled_paths[data_path], SEQUENCE_LEN, mean, std) 
+                               for data_path in train_paths]
 
-        while any(assembler.ended == False for assembler in sequence_assemblers):
-            available_assemblers = [assembler for assembler in sequence_assemblers if assembler.ended == False]
+        while any(assembler.ended == False for assembler in train_sequence_assemblers):
+            available_assemblers = [assembler for assembler in train_sequence_assemblers if assembler.ended == False]
             selected_assembler = random.choice(available_assemblers)
             sequence, label = selected_assembler.get_sequence()
 
@@ -154,7 +175,8 @@ def train():
             if batch_count % log_interval == 0:
                 _, acc = accuracy.get()
                 _, met = metric.get()
-                print(f"""Epoch[{epoch + 1}] Batch[{batch_count}] mse = {met} | accuracy = {acc}""")
+                #val_acc, val_met = evaluate_val(val_fold, train_labeled_paths, net, SEQUENCE_LEN, mean, std)
+                print(f"""Epoch[{epoch + 1}] Batch[{batch_count}] train mse = {round(met, 5)} | train accuracy = {round(acc, 5)}""")
 
 
         if batch.shape[0] != 0:
@@ -168,9 +190,9 @@ def train():
 
         _, acc = accuracy.get()
         _, met = metric.get()
-        test_acc, test_met = evaluate_test(test_data_paths, net, SEQUENCE_LEN, mean, std)
+        test_acc, test_met = evaluate_test(test_labeled_paths, net, SEQUENCE_LEN, mean, std)
         print(" ")
-        print(f"[Epoch {epoch + 1}] training mse={met} | validation mse={test_met} | validation accuracy={test_acc}")
+        print(f"Epoch[{epoch + 1}] train mse={round(met, 5)} | test mse={test_met} | test accuracy={test_acc}")
         print(" ")
 
     net.save("stair_safety_LSTM_only")
